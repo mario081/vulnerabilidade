f647bf2b2efb7a2533f6d957b4e3e4dfc245e410
mario081 <ffa35922@gmail.com>
2025-04-06 23:48:09 -0300

    vulnerabilidade

---
+++ server.js
@@ -0,0 +1,80 @@
+import express from 'express'
+import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+<<<<<<< HEAD
+            name: req.body.name,
+            age: req.body.age
+=======
+            senha: req.body.senha,
+            name: req.body.name
+>>>>>>> dc159dd (Vulnerabilidade)
+        }
+    })
+    res.status(201).json(req.body)
+})
+
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(users)
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+    //console.log(req.body);
+
+    let body = req.body
+    let update = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+<<<<<<< HEAD
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+=======
+            senha: req.body.senha,
+            name: req.body.name
+>>>>>>> dc159dd (Vulnerabilidade)
+        }
+    })
+
+    res.status(201).json(update)
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    try {
+        await prisma.user.delete({
+            where: {
+                id: req.params.id,
+            },
+        })
+        res.status(200).json({ message: 'Deletado com sucesso!' })
+    } catch {
+        res.status(403).json({ message: 'usuario ja Deletado!' })
+    }
+})
+
+
+app.listen(3000)
+
+
+/*  
+    users do bd: ffa35922
+    password do bd: dwu19YeJYVL0IqVN
+*/
